9a0545de219133e7206c502e54b505845df906bb
Ashley Berry <ashleywberry@gmail.com>
2020-11-11 10:24:52 -0600

    updated rules image to be vertical placeholder

--- src/components/Admin/EditRules/EditRules.js
+++ src/components/Admin/EditRules/EditRules.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../../redux/mapStoreToProps';
 import { Grid } from "@material-ui/core";
-import Placeholder from '../../../images/placeholder-square.png';
+import Placeholder from '../../../images/placeholder-vertical.jpg';
 import EditFAQ from '../EditFAQ/EditFAQ.js';
 import './EditRules.css';
 
