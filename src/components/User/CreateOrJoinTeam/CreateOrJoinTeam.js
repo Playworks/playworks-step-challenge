444f1bca62017fba15ae83f9a1731cd1c6ee5bbe
Ashley Berry <ashleywberry@gmail.com>
2020-11-12 11:39:30 -0600

    log out button stays at bottom

--- src/components/User/CreateOrJoinTeam/CreateOrJoinTeam.js
+++ src/components/User/CreateOrJoinTeam/CreateOrJoinTeam.js
@@ -32,12 +32,14 @@ class CreateOrJoinTeam extends Component {
           <div onClick={this.joinTeam}>
             <JoinTeamLink/>
           </div>
-          <button
-            type="button"
-            className="btn btn_asLink"
-            onClick={() => this.props.dispatch({ type: 'LOGOUT' })}>
-            Log Out
-          </button>
+          <div id='footer'>
+            <button
+              type="button"
+              className="btn btn_asLink"
+              onClick={() => this.props.dispatch({ type: 'LOGOUT' })}>
+              Log Out
+            </button>
+          </div>
         </center>
       </div>
     );
