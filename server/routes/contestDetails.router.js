efd07d0240ece99b4d09437896927101ecb63973
Arthur Carson <rthrcrsn@gmail.com>
2020-11-16 13:59:07 -0600

    Work in progress admin stuff

--- server/routes/contestDetails.router.js
+++ server/routes/contestDetails.router.js
@@ -10,6 +10,7 @@ router.get('/:id', (req, res) => {
   // GET route code here
   console.log('req', req.params.id);
   let contestToGet = req.params.id;
+  
   // query gets team captain's full name and team name
   let queryText = `
     SELECT * FROM "user"
@@ -19,18 +20,18 @@ router.get('/:id', (req, res) => {
     ON "user"."teams_id" = "teams"."id"
     JOIN "photos"
     ON "photos"."user_id" = "user"."id"
-    WHERE "user"."contests_id" = 1;
+    WHERE "user"."contests_id" = $1;
   `;
   pool.query(queryText, [contestToGet])
   .then(response => {
       console.log('resp', response.rows);
-      
       res.send(response.rows)
   })
   .catch((error) => {
       console.log('error contest details get', error);
       res.sendStatus(500);
   })
+  
 });
 
 /**
