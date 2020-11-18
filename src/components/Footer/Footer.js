95f4d7ad3d33823cd4c84ad9c0d487e8a8e8b5c5
David Gould <davidrichardgould93@gmail.com>
2020-11-18 09:43:26 -0600

    favicon and updated app name

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,6 +8,7 @@ import HomeIcon from '@material-ui/icons/Home';
 import DirectionsWalkIcon from '@material-ui/icons/DirectionsWalk';
 import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
 import { Typography } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
 
 class Footer extends Component {
 
@@ -15,22 +16,22 @@ class Footer extends Component {
     return (
       <div className='footerContainer'>
         <center>
-        <div className='footerIconGroup' onClick={() => {this.props.history.push('/home')}}>
-          <HomeIcon/>
-          <Typography variant='body2'>Home</Typography>
-        </div>
-        <div className='footerIconGroup' onClick={() => {this.props.history.push('/addphoto')}}>
-          <AddAPhotoIcon/>
-          <Typography variant='body2'>Add Photo</Typography>
-        </div>
-        <div className='footerIconGroup' onClick={() => {this.props.history.push('/addsteps')}}>
-          <DirectionsWalkIcon/>
-          <Typography variant='body2'>Add Steps</Typography>
-        </div>
-        <div className='footerIconGroup' onClick={() => {this.props.history.push('/team')}}>
-          <SupervisedUserCircleIcon/>
-          <Typography variant='body2'>Team Home</Typography>
-        </div>
+          <div className='footerIconGroup' onClick={() => {this.props.history.push('/home')}}>
+            <HomeIcon/>
+            <Typography variant='body2'>Home</Typography>
+          </div>
+          <div className='footerIconGroup' onClick={() => {this.props.history.push('/addphoto')}}>
+            <AddAPhotoIcon/>
+            <Typography variant='body2'>Add Photo</Typography>
+          </div>
+          <div className='footerIconGroup' onClick={() => {this.props.history.push('/addsteps')}}>
+            <DirectionsWalkIcon/>
+            <Typography variant='body2'>Add Steps</Typography>
+          </div>
+          <div className='footerIconGroup' onClick={() => {this.props.history.push('/team')}}>
+            <SupervisedUserCircleIcon/>
+            <Typography variant='body2'>Team Home</Typography>
+          </div>
         </center>
       </div>
     );
