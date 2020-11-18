d5b3c9a68355d31147f62facddc8f9ecc6c31d67
David Gould <davidrichardgould93@gmail.com>
2020-11-18 14:08:04 -0600

    updated sql query to show only approved photos on home page and added where you can see team and company name

--- src/components/User/AllChallengePhotos/AllChallengeFeedInfo.js
+++ src/components/User/AllChallengePhotos/AllChallengeFeedInfo.js
@@ -6,20 +6,22 @@ import { Grid, Typography } from '@material-ui/core';
 class AllChallengeFeedInfo extends Component {
 
   render() {
+    console.log('this is props in allchallengefeedinfo', this.props);
     return (
         <div className='feedInfo'>
             <Grid container spacing={1}>
             <Grid container item xs={12} spacing={3}>
                 <Grid item xs={3}>
                 <div className='feedAvatarDiv'>
-                    <img className='feedAvatar' src={this.props.photo.image_path}/>
+                    <img className='feedAvatar' src={this.props.photo.user_image_path}/>
                 </div>
                 </Grid>
                 <Grid item xs={9}>
                 <div className='feedDescription'>
                     <Typography variant='body2'>{this.props.photo.username}</Typography>
-                    <Typography variant='body2'>TEAM NAME HERE</Typography>
-                    <Typography variant='body2'>{this.props.photo.name}</Typography>
+                    <Typography variant='body2'>{this.props.photo.team_name}</Typography>
+                    <Typography variant='body2'>{this.props.photo.company_name}</Typography>
+                    <Typography variant='body2'>{this.props.photo.challenge_name}</Typography>
                 </div>
                 </Grid>
             </Grid>
